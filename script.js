d94f8cf31fee676ef6b980be4d051343a48122f0
raghavgupta2u <raghav.gupta7rg@gmail.com>
2024-08-10 19:03:11 +0530

    add

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ var icon =document.getElementById("icon");
 icon.onclick = function(){
     document.body.classList.toggle("dark");
     if(document.body.classList.contains("dark")){
-        icon.sec = "assets/sun.png";
+        icon.src = "assets/sun.png";
     }else{
         icon.src = "assets/moon.png";
     }
