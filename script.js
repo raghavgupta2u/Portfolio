d762819d66bab7d9d7c91c923d044821237c73d8
raghavgupta2u <raghav.gupta7rg@gmail.com>
2024-08-10 18:11:43 +0530

    add

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-link");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+var icon =document.getElementById("icon");
+
+icon.onclick = function(){
+    document.body.classList.toggle("dark");
+    if(document.body.classList.contains("dark")){
+        icon.sec = "assets/sun.png";
+    }else{
+        icon.src = "assets/moon.png";
+    }
+}
