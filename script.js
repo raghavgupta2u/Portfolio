1c37e906522f4789c5d0ec9e8db7c2b1f8b2906d
raghavgupta2u <raghav.gupta7rg@gmail.com>
2024-08-16 18:18:16 +0530

    add

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-function toggleMenu(){
-    const menu = document.querySelector(".menu-link");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
-
 var icon =document.getElementById("icon");
 
 icon.onclick = function(){
